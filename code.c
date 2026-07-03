3109e59fd0a52a60564bfceaf1f5502c340138ab
FelipeVivan <vivan.felipeo@gmail.com>
2026-07-03 20:24:26 -0300

    0.2

--- code.c
+++ code.c
@@ -4,6 +4,17 @@
 
 int main() {
     const int c[6]={22,18,15,13,10,8}, tanque=50;
+    float liters, range;
+    while(1){
+        scanf("%f", &liters);
+        if(0<=liters || 50>=liters){
+        break;
+        }else{
+        printf("Valor inválido, digite um valor entre 0 e 50");
+        }
+        
+    } 
+    
     
     return 0;
 }
