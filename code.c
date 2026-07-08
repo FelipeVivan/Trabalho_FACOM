0b8dee3ebf75dad65beeb69f8e1f2ef7d23c0525
FelipeVivan <vivan.felipeo@gmail.com>
2026-07-08 17:25:48 -0300

    0.4

--- code.c
+++ code.c
@@ -6,22 +6,19 @@
 int main() {
     const int c[6]={22,18,15,13,10,8}, tanque=50;
     float liters, range;
+    char a;
     while(1){
         printf("Digite o combustivel disponivel:");
-        if ((scanf("%f", &liters) == 1) && (liters>0 && liters<=tanque)){
-            int ch=getchar();
-            if (ch == '\r')
-            ch=getchar();
-            if (ch == '\n'){
-            break;
-            }else{
-                printf("\nValor invalido! Digite um valor valido.\n");
-            }
-        }else{
+        if ((scanf("%f%c", &liters, &a) != 2) || (a!='\n') || ((liters<=0) || (liters>tanque))){
             int ch;
             while ((ch = getchar()) != '\n' && ch != EOF);
-            printf("\nValor invalido! Digite um valor valido.\n");
-        }    
-    }   
+            printf("\nValor invalido! Digite um valor valido.\n");            
+            
+        }else{
+            printf("aaaaa");
+            break;
+        }
+    }    
+    
     return 0;
 }
