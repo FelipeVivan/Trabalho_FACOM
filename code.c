241c90d77180e70d87ab4fa2868bab8fb0bb227e
FelipeVivan <vivan.felipeo@gmail.com>
2026-07-12 14:31:01 -0300

    0

--- code.c
+++ code.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include <math.h>
 
+
+
 int main() {
     const int c[6]={22,18,15,13,10,8}, tanque=50;
     float liters, range;
-    int pot;
+    int pot, i, z=1;
     char a;
     while(1){
         printf("Digite o combustivel disponivel:");
@@ -22,46 +24,23 @@ int main() {
             break;
         }
     }
-    int i=0;
-    while(i != 1){
-        printf("\nDigite a potencia utilizada:");
-        scanf("%d", &pot);
-        switch (pot){
-        case 20: 
-               printf("O consumo e %dKm/L", c[0]);
-               range = liters * c[0];
-               i = 1;
-               break;
-        case 40:
-               printf("O consumo e %dKm/L", c[0]);
-               range = liters * c[0];
-               i = 1;
-               break;
-        case 50:
-               printf("O consumo e %dKm/L", c[0]);
-               range = liters * c[0];
-               i=1;
-               break;
-        case 60:
-               printf("O consumo e %dKm/L", c[0]);
-               range = liters * c[0];
-               i=1;
-               break;
-        case 80:
-               printf("O consumo e %dKm/L", c[0]);
-               range = liters * c[0];
-               i=1;
-               break;
-        case 100:
-               printf("O consumo e %dKm/L", c[0]);
-               range = liters * c[0];
-               i=1;
-               break;
-        default:
-            printf("Valor invalido, Digite um valor entre estes: 20, 40, 50, 60, 80, 100 ");
-        }  
+    while (z == 1){
+        printf("\nDigite a potencia que sera utilizada:");
+        if (scanf("%d", &pot) != 1) {
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF);
+            printf("Entrada invalida\n");
+            continue;
     }
-    printf("\n\nAAAAAA");    
-    
+
+    for (i=0; i<6; i++){
+        if (c[i]==pot){
+            z=0;
+            break;
+        }               
+    }
+    printf("Valor invalido. Digite um valor entre estes: 20, 40, 50, 60, 80, 100\n");
+    }
+
     return 0;
 }
