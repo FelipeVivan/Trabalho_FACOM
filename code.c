d9d27701745487c52f7c336254b2cc75070f55a6
FelipeVivan <vivan.felipeo@gmail.com>
2026-07-12 14:54:37 -0300

    0

--- code.c
+++ code.c
@@ -6,42 +6,51 @@
 
 
 int main() {
-    const int c[6]={22,18,15,13,10,8}, tanque=50; int potencias[6]={20,40,50,60,80,100};
+    const int c[6]={22,18,15,13,10,8}, tanque=50; int potencias[6]={20,40,50,60,80,100}; int alerta=25;
     float liters, range;
     int pot, i, z=1;
     char a;
     while(1){
         printf("Digite o combustivel disponivel:");
         a = '\0';
-        if ((scanf("%f%c", &liters, &a) != 2) || (a!='\n') || ((liters<0) || (liters>tanque))){
+        if ((scanf("%f%c", &liters, &a) != 2) || (a!='\n') || ((liters<=0) || (liters>tanque))){
             if (a != '\n'){
             int ch;
             while ((ch = getchar()) != '\n' && ch != EOF);
             }
             printf("\nValor invalido! Digite um valor valido.\n");                        
         }else{
-            printf("aaaaa");
             break;
         }
     }
     while (z == 1){
         printf("\nDigite a potencia que sera utilizada:");
+        a = '\0';
         if (scanf("%d", &pot) != 1) {
+            if (a != '\n'){
             int ch;
             while ((ch = getchar()) != '\n' && ch != EOF);
+            }
             printf("Entrada invalida\n");
             continue;
-    }
+        }
 
-    for (i=0; i<6; i++){
-        if (potencias[i]==pot){
-            z=0;
-            break;
-        }               
+        for (i=0; i<6; i++){
+            if (potencias[i]==pot){
+               z=0;
+               break;
+            }               
+        }
+         if (z==1){
+              printf("Valor invalido. Digite um valor entre estes: 20, 40, 50, 60, 80, 100\n");
+         }
     }
-    if (z==1){
-        printf("Valor invalido. Digite um valor entre estes: 20, 40, 50, 60, 80, 100\n");
-    }
+    range = c[i] * liters;
+    printf("A sua autonomia e de: %f Km", range);
+    if (range < alerta)
+    printf("\nAlerta de autonomia baixa ");     
 
+
+    
     return 0;
 }
